256be46dd0d60c8b82fa335a12ab924d356ec2ae
juzerx <juamca2007@gmail.com>
2023-03-22 21:03:28 -0500

    new changes

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,15 @@
-const present = document.getElementById("presentation");
-const text = document.getElementsByClassName("t_press");
-    present.onmouseover = function () {
-        
-    }
+// const present = document.getElementById("presentation");
+// const text = document.getElementById("t_pres");
+// const texti = document.getElementById("t_pInfo");
+
+// function show() {
+//     text.hidden = true;
+//     texti.hidden = false;
+//     console.log(text);
+// }
+// function hide() {
+//     text.hidden = false;
+//     texti.hidden = true;
+//     console.log(text);
+    
+// }
